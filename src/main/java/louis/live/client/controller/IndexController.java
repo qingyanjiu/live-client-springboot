805d7eff58d05bc04f686fdb0cf7197b928633ec
louisliu <louisliu@cisco.com>
2017-09-06 14:47:25 +0800

    added page and static files

--- src/main/java/louis/live/client/controller/IndexController.java
+++ src/main/java/louis/live/client/controller/IndexController.java
@@ -1,14 +1,22 @@
 package louis.live.client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+//@PropertySource(value = {"classpath:application.properties"},encoding="utf-8")
 public class IndexController {
 
+    @Value("${live.client.title}")
+    private String title;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("title",title);
         return "index";
     }
 }
