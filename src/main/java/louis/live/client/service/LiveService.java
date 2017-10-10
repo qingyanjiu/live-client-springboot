7e511109ce6168362e8e4a9280e00cbc99cc8c63
louisliu <louisliu@cisco.com>
2017-10-10 13:03:31 +0800

    added live service methods

--- src/main/java/louis/live/client/service/LiveService.java
+++ src/main/java/louis/live/client/service/LiveService.java
@@ -1,9 +1,8 @@
 package louis.live.client.service;
 
 import louis.live.client.mapper.LiveMapper;
-import louis.live.client.mapper.UserMapper;
+import louis.live.client.vo.Live;
 import louis.live.client.vo.LiveInfo;
-import louis.live.client.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +18,35 @@ public class LiveService {
         return liveMapper.getAll();
     }
 
+    public void add(Live live) {
+        liveMapper.add(live);
+    }
 
+    public void delete(Map params){
+        liveMapper.delete(params);
+    }
+
+    public void end(Map params){
+        liveMapper.end(params);
+    }
+
+    public Live getActiveLiveOfUser(Map params){
+        return liveMapper.getActiveLiveOfUser(params);
+    }
+
+    public List<Live> getHistoryLivesOfUser(Map params){
+        return liveMapper.getHistoryLivesOfUser(params);
+    }
+
+    public List<Live> getLiveByName(Map params){
+        return liveMapper.getLiveByName(params);
+    }
+
+    public void updateLiveName(Map params){
+        liveMapper.updateLiveName(params);
+    }
+
+    public void updateLivePassword(Map params){
+        liveMapper.updateLivePassword(params);
+    }
 }
